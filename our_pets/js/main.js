99593d34b745b1129bce1282ca7f6b1e9f5af1f1
Artem <sfmillingg@gmail.com>
2023-06-21 17:43:25 +0400

    add project

---
+++ our_pets/js/main.js
@@ -0,0 +1,57 @@
+console.log("Баллы: 40/40")
+
+const linksGet = document.querySelectorAll('a[href^="#"]');
+
+linksGet.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const linkScrool = this.getAttribute('href');
+
+    document.querySelector(linkScrool).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Burger menu
+
+(function () {
+  const burgerIcon = document.querySelector('.burger');
+  const menu = document.querySelector('.header__nav');
+  const shadow = document.querySelector('.shadow')
+  const links = document.querySelector('.header__nav-items');
+  const body = document.querySelector('.body');
+  let turn = 0;
+
+burgerIcon.addEventListener('click', () => {
+  menu.classList.toggle('header__nav-active');
+  if(turn === 0) {
+    turn = 90;
+    burgerIcon.style.transform = `rotate(${turn}deg)`;
+    body.style.overflow = 'hidden';
+    shadow.style.display = 'block';
+  } else {
+    turn = 0;
+    burgerIcon.style.transform = `rotate(${turn}deg)`;
+    body.style.overflow = 'visible';
+    shadow.style.display = 'none';
+  }
+  links.addEventListener('click', () => {
+    turn = 90;
+    menu.classList.remove('header__nav-active')
+    if(turn === 90) {
+      turn = 0;
+      burgerIcon.style.transform = `rotate(${turn}deg)`;
+      body.style.overflow = 'visible';
+      shadow.style.display = 'none';
+    }
+  })
+})
+}());
+
+
+
+
+
+
